203426e0f3426d721636fcd40ab7a751ef119c9f
Stanislav <dr.arntholcz@mail.ru>
2021-02-25 13:52:21 +0700

    Renamed functions and metods.

--- js/my.js
+++ js/my.js
@@ -1,29 +1,12 @@
 'use strict';
 ///////////create object massive////////////
 const products = [
-    { id: 1, title: 'Notebook', price: 20000, img_src: "img/1.jpg" },
+    { id: 1, title: 'Notebook', price: 20000, imgSrc: "img/1.jpg" },
     { id: 2, title: 'Mouse', price: 1500 },
-    { id: 3, title: 'Keyboard', price: 5000, img_src: 'img/3.jpg' },
+    { id: 3, title: 'Keyboard', price: 5000, imgSrc: 'img/3.jpg' },
     { id: 4, title: 'Gamepad', price: 4500 },
 ];
 ///////////create object massive////////////
-////////////////create function for added products//////////////////
-function blabla(targetTag, targetObj) {
-    let insert = '',
-        img_src = '';
-    for (let i = 0; i < targetObj.length; i++) {
-        if (typeof targetObj[i].img_src === "undefined") { img_src = "img/" + targetObj[i].title + ".jpg" } else { img_src = targetObj[i].img_src };
-        insert += `<div class="product-item">
-    <img class="product-item-img" src="${img_src}">
-                <h3>${targetObj[i].title}</h3>
-                <p>${targetObj[i].price}</p>
-                <button class="by-btn">Добавить в корзину</button>
-              </div>`;
-    };
-    targetTag.innerHTML = insert;
-};
-////////////////create function for added products//////////////////
-blabla(document.querySelector('.products'), products);
 
 ///////////init class basket///////////////////////
 class basket {
@@ -32,7 +15,7 @@ class basket {
             this.listCount = this.listProducts.length;
         }
         ////////////metod calculate products
-    bskt_sum() {
+    basket_sum() {
             this.sum = 0;
             this.listProducts.forEach((element) => {
                 this.sum += element.price
@@ -40,7 +23,7 @@ class basket {
             return this.sum;
         }
         ///////////////metod deleted products in basket//////
-    bskt_prd_del() {}
+    deleted_product_in_basket() {}
 }
 ///////////init class basket///////////////////////
 ///////////init class basket_item//////////////////
@@ -52,24 +35,43 @@ class basket_item {
 class products_item {
     constructor() {}
         ///////////////metod apply even 'click' to buttons///////////////////////
-    prdts_add_bskt() {
-            let btnsPdts = document.querySelectorAll('.product-item > .by-btn');
-            let summaTotal = document.querySelector('.summaTotal');
-            for (let i = 0; i < btnsPdts.length; i++) {
-                btnsPdts[i].addEventListener('click', (event) => {
+    add_product_to_basket() {
+            let ButtonProducts = document.querySelectorAll('.product-item > .by-btn');
+            let SummaTotal = document.querySelector('.SummaTotal');
+            for (let i = 0; i < ButtonProducts.length; i++) {
+                ButtonProducts[i].addEventListener('click', (event) => {
                     if (event.target.textContent === 'Добавить в корзину') {
                         event.target.textContent = 'Добавлено';
-                        bsk.listProducts.push(products[i]);
-                        summaTotal.innerHTML = `Итого: ${bsk.bskt_sum()}р.`;
+                        basket1.listProducts.push(products[i]);
+                        SummaTotal.innerHTML = `Итого: ${basket1.basket_sum()}р.`;
                     }
                 });
 
             }
         }
         ///////////////metod apply even 'click' to buttons///////////////////////
+        ////////////////create metod for added products//////////////////
+    add_product_to_list(targetTag, targetObj) {
+        let insert = '',
+            imgSrc = '';
+        for (let i = 0; i < targetObj.length; i++) {
+            if (typeof targetObj[i].imgSrc === "undefined") { imgSrc = "img/" + targetObj[i].title + ".jpg" } else { imgSrc = targetObj[i].imgSrc };
+            insert += `<div class="product-item">
+    <img class="product-item-img" src="${imgSrc}">
+                <h3>${targetObj[i].title}</h3>
+                <p>${targetObj[i].price}</p>
+                <button class="by-btn">Добавить в корзину</button>
+              </div>`;
+        };
+        targetTag.innerHTML = insert;
+    };
+    ////////////////create metod for added products//////////////////
 }
 ///////////init class products_item//////////////////
 ///////////create object basket//////////////////
-let bsk = new basket(),
-    prdts_item = new products_item();
-prdts_item.prdts_add_bskt();
+let basket1 = new basket(),
+    ///////////create object products_item//////////////////
+    productItem1 = new products_item();
+
+productItem1.add_product_to_list(document.querySelector('.products'), products);
+productItem1.add_product_to_basket();
